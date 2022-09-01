1d5aef0caf1a8e5adfc9194e34ec2a5672411b20
ferdar7 <ferdar7@gmail.com>
2022-09-01 14:16:05 +0700

    updated settings.py

--- PBP/settings.py
+++ PBP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*_6h%gt^3!7(eov3@+@&6ox7tk8g9k(i6h^c^bne_n(b2a+0*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PBP.urls'
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
